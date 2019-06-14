f862eaf5bdf77d72157e8cd017409cd621d0b7f0
progane137 <parkseok138@gmail.com>
2019-06-14 14:14:00 +0900

    6/14 2시커밋

--- src/main/java/com/shoestar/client/myPage/service/MyPageServiceImpl.java
+++ src/main/java/com/shoestar/client/myPage/service/MyPageServiceImpl.java
@@ -1,8 +1,27 @@
 package com.shoestar.client.myPage.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.shoestar.client.orders.vo.OrdersInsVO;
+import com.shoestar.client.orders.vo.OrdersVO;
 
 @Service
 public class MyPageServiceImpl implements MyPageService{
+
+	@Override
+	public List<OrdersVO> ordersDataByMemNo(int mem_no) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<OrdersInsVO> ordersInsByOdNo(OrdersVO ovo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
 
 }
