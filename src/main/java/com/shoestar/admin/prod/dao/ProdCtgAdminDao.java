6078d5ae76e0af04d402769e2930fc2f5d389c8b
낡은것 <jhbae0117@naver.com>
2019-06-17 00:10:48 +0900

    배정훈 06-16 마지막

---
+++ src/main/java/com/shoestar/admin/prod/dao/ProdCtgAdminDao.java
@@ -0,0 +1,27 @@
+package com.shoestar.admin.prod.dao;
+
+import java.util.List;
+
+import com.shoestar.client.prod.vo.ProdColorVO;
+import com.shoestar.client.prod.vo.ProdCtgVO;
+
+public interface ProdCtgAdminDao {
+	
+	// 카테고리
+	public List<ProdCtgVO> ctgList();
+	
+	public int insertCtg(ProdCtgVO ctvo);
+	
+	public int updateCtg(ProdCtgVO ctvo);
+	
+	public int deleteCtg(ProdCtgVO ctvo);
+	
+	// 색상
+	public List<ProdColorVO> colorList();
+	
+	public int insertColor(ProdColorVO clvo);
+	
+	public int updateColor(ProdColorVO clvo);
+	
+	public int deleteColor(ProdColorVO clvo);
+}
